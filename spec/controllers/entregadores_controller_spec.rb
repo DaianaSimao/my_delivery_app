60b63348d43ac8e8f1f2b8fcc4c058e85b3f27d4
DaianaSimao <daiana.xd11@hotmail.com>
2025-02-03 22:06:17 -0300

    Fix: Exclusão de testes desnecessarios.

--- spec/controllers/entregadores_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe EntregadoresController, type: :controller do
-
-end
--- spec/controllers/itens_pedidos_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ItensPedidosController, type: :controller do
-
-end
