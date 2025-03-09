349f98f3e9ec4232d43ceb3d9491ab2a22150aec
DaianaSimao <daiana.xd11@hotmail.com>
2025-03-09 16:25:31 -0300

    feat: Adiciona tratamento de erros nas funções de busca e criação da API

--- frontend-cliente/src/services/api.ts
+++ frontend-cliente/src/services/api.ts
@@ -6,25 +6,42 @@ const api: AxiosInstance = axios.create({
 });
 
 export const fetchMenu = async (restauranteId: string | number) => {
-  const response = await api.get(`/api/v1/cardapio/${restauranteId}`);
-
-  return response.data.data;
+  try {
+    const response = await api.get(`/api/v1/cardapio/${restauranteId}`);
+    return response.data.data;
+  }
+  catch (error) {
+    console.error("Erro ao buscar cardápio:", error);
+    throw error;
+  }
 };
 
 export const fetchProductDetails = async (id: number) => {
-  const response = await api.get(`/api/v1/produtos/${id}`);
-  return response.data.data;
+  try {
+    const response = await api.get(`/api/v1/produtos/${id}`);
+    return response.data.data;
+  }
+  catch (error) {
+    console.error("Erro ao buscar detalhes do produto:", error);
+    throw error;
+  }
 };
 
 export const fetchRestaurantInfo = async (restauranteId: string | number) => {
-  const response = await api.get(`/api/v1/restaurantes/${restauranteId}`);
-  return response.data.data; // Retorna as informações do restaurante
+  try {
+    const response = await api.get(`/api/v1/restaurantes/${restauranteId}`);
+    return response.data.data;
+  }
+  catch (error) {
+    console.error("Erro ao buscar informações do restaurante:", error);
+    throw error
+  }
 };
 
 export const fetchClienteByWhatsApp = async (whatsapp: string) => {
   try {
     const response = await api.get(`/api/v1/clientes/?whatsapp=${whatsapp}`);
-    return response.data; // Retorna os dados do client
+    return response.data;
   } catch (error) {
     console.error("Erro ao buscar cliente:", error);
     throw error;
@@ -34,18 +51,17 @@ export const fetchClienteByWhatsApp = async (whatsapp: string) => {
 export const fetchEnderecoById = async (enderecoId: number) => {
   try {
     const response = await api.get(`/api/v1/enderecos/${enderecoId}`);
-    return response.data; // Retorna os dados do endereço
+    return response.data;
   } catch (error) {
     console.error("Erro ao buscar endereço:", error);
     throw error;
   }
 };
 
-// Novas funções
 export const criarCliente = async (clienteData: any) => {
   try {
     const response = await api.post("/api/v1/clientes", clienteData);
-    return response.data; // Retorna o cliente criado
+    return response.data;
   } catch (error) {
     console.error("Erro ao criar cliente:", error);
     return null;
@@ -55,7 +71,7 @@ export const criarCliente = async (clienteData: any) => {
 export const criarEndereco = async (dadosEndereco: any) => {
   try {
     const response = await api.post('/api/v1/enderecos', dadosEndereco);
-    return response.data; // Retorna o endereço criado
+    return response.data
   } catch (error) {
     console.error('Erro ao criar endereço:', error);
     throw error;
@@ -65,7 +81,7 @@ export const criarEndereco = async (dadosEndereco: any) => {
 export const criarPedido = async (pedidoData: any) => {
   try {
     const response = await api.post("/api/v1/pedidos", pedidoData);
-    return response.data; // Retorna o pedido criado
+    return response.data;
   } catch (error) {
     console.error("Erro ao criar pedido:", error);
     throw error;
@@ -75,27 +91,27 @@ export const criarPedido = async (pedidoData: any) => {
 export const atualizarCliente = async (clienteId: string, dadosAtualizados: { nome: string; telefone: string; }) => {
   try {
     const response = await api.put(`/api/v1/clientes/${clienteId}`, dadosAtualizados);
-    return response.data; // Retorna o cliente atualizado
+    return response.data;
   } catch (error) {
     console.error("Erro ao atualizar cliente:", error);
-    throw error; // Lança o erro para ser tratado no componente
+    throw error;
   }
 };
 
 export const atualizarEnderecoCliente = async (clienteId: string, dadosAtualizados: { endereco_id: string; }) => {
   try {
     const response = await api.put(`/api/v1/clientes/${clienteId}`, dadosAtualizados);
-    return response.data; // Retorna o cliente atualizado
+    return response.data;
   } catch (error) {
     console.error("Erro ao atualizar cliente:", error);
-    throw error; // Lança o erro para ser tratado no componente
+    throw error;
   }
 };
 
 export const atualizarEndereco = async (enderecoId: number, dadosAtualizados: any) => {
   try {
     const response = await api.put(`/api/v1/enderecos/${enderecoId}`, dadosAtualizados);
-    return response.data; // Retorna o endereço atualizado
+    return response.data;
   } catch (error) {
     console.error('Erro ao atualizar endereço:', error);
     throw error;
