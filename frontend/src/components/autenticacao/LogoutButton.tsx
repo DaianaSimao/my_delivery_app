f6cb8dd6ebf5ad081a095d282a82022a69583cdd
DaianaSimao <daiana.xd11@hotmail.com>
2025-03-13 21:45:14 -0300

    feat: Adiciona novos ícones SVG para o menu e botão de logout

--- frontend/src/components/autenticacao/LogoutButton.tsx
+++ frontend/src/components/autenticacao/LogoutButton.tsx
@@ -3,6 +3,7 @@ import axios from "axios"; // Importando o axios
 import { useAuth } from "../../context/useAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import logoutIcon from "/icons/logout.svg";
 
 const LogoutButton: React.FC = () => {
   const { logout } = useAuth();
@@ -54,19 +55,11 @@ const LogoutButton: React.FC = () => {
             onClick={handleLogout}
             className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
           >
-            <svg
-              className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-            >
-              <path
-                fillRule="evenodd"
-                d="M3 10a1 1 0 0 1 1-1h8.586l-3.293-3.293a1 1 0 1 1 1.414-1.414l5 5a1 1 0 0 1 0 1.414l-5 5a1 1 0 1 1-1.414-1.414L12.586 11H4a1 1 0 0 1-1-1z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <img
+              src={logoutIcon}
+              alt="Ícone do pagamento"
+              className="h-8 w-auto mr-3 text-4xl" 
+            />
             <span className="flex-1 ms-3 whitespace-nowrap">Logout</span>
           </a>
         </li>
