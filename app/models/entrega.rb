864b1b135c34e3ba4341c14be24ebfbf43d9d05c
DaianaSimao <daiana.xd11@hotmail.com>
2025-03-15 12:46:06 -0300

    feat: Atualiza status do pedido ao marcar entrega como 'Entregue'

--- app/models/entrega.rb
+++ app/models/entrega.rb
@@ -1,4 +1,12 @@
 class Entrega < ApplicationRecord
   belongs_to :pedido
   belongs_to :entregador, optional: true
+
+  after_update :atualizar_status_pedido, if: :status_changed?
+
+  def atualizar_status_pedido
+    if self.status == "Entregue"
+      self.pedido.update(status: "Finalizado")
+    end
+  end
 end
