5ba6cb9135e87cfa5bc31dc6837361e61012343c
DaianaSimao <daiana.xd11@hotmail.com>
2025-03-28 19:25:38 -0300

    feat: Remove versão da API das rotas de atualização e busca de dados

--- frontend-cliente/src/services/api.ts
+++ frontend-cliente/src/services/api.ts
@@ -109,7 +109,7 @@ export const atualizarCliente = async (clienteId: string, dadosAtualizados: { no
 
 export const atualizarEnderecoCliente = async (clienteId: string, dadosAtualizados: { endereco_id: string; }) => {
   try {
-    const response = await api.put(`/api/v1/clientes/${clienteId}`, dadosAtualizados);
+    const response = await api.put(`/clientes/${clienteId}`, dadosAtualizados);
     return response.data;
   } catch (error) {
     console.error("Erro ao atualizar cliente:", error);
@@ -119,7 +119,7 @@ export const atualizarEnderecoCliente = async (clienteId: string, dadosAtualizad
 
 export const atualizarEndereco = async (enderecoId: number, dadosAtualizados: any) => {
   try {
-    const response = await api.put(`/api/v1/enderecos/${enderecoId}`, dadosAtualizados);
+    const response = await api.put(`/enderecos/${enderecoId}`, dadosAtualizados);
     return response.data;
   } catch (error) {
     console.error('Erro ao atualizar endereço:', error);
@@ -129,7 +129,7 @@ export const atualizarEndereco = async (enderecoId: number, dadosAtualizados: an
 
 export const fetchRegioesEntrega = async (restauranteId: string | number) => {
   try {
-    const response = await api.get(`/api/v1/restaurantes/regioes_entrega/${restauranteId}`);
+    const response = await api.get(`/restaurantes/regioes_entrega/${restauranteId}`);
     console.log(response.data);
     return response.data;
   } catch (error) {
@@ -140,7 +140,7 @@ export const fetchRegioesEntrega = async (restauranteId: string | number) => {
 
 export const fetchMaisPedidos = async (restauranteId: string | number) => {
   try {
-    const response = await api.get(`/api/v1/restaurantes/${restauranteId}/mais_pedidos`);
+    const response = await api.get(`/restaurantes/${restauranteId}/mais_pedidos`);
     return response.data.data;
   } catch (error) {
     console.error("Erro ao buscar itens mais pedidos:", error);
@@ -150,7 +150,7 @@ export const fetchMaisPedidos = async (restauranteId: string | number) => {
 
 export const fetchSecoes = async (restauranteId: string | number) => {
   try {
-    const response = await api.get(`/api/v1/restaurantes/${restauranteId}/secoes`);
+    const response = await api.get(`/restaurantes/${restauranteId}/secoes`);
     return response.data.data;
   } catch (error) {
     console.error("Erro ao buscar seções do cardápio:", error);
@@ -160,7 +160,7 @@ export const fetchSecoes = async (restauranteId: string | number) => {
 
 export const fetchPromocoesAtivas = async (restauranteId: string) => {
   try {  
-    const response = await api.get(`/api/v1/restaurantes/${restauranteId}/promocoes`);
+    const response = await api.get(`/restaurantes/${restauranteId}/promocoes`);
     return response.data.data;
   } catch (error) {
     console.error("Erro ao buscar promoções ativas:", error);
