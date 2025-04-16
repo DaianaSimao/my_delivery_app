a2ba076bb0ece0aa019555324019e45de3dd06f6
DaianaSimao <daiana.xd11@hotmail.com>
2025-04-16 16:43:53 -0300

    refac: Adiciona criação de bancos de dados para produção e melhora a conexão com PostgreSQL

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -1,9 +1,16 @@
 Rails.application.config.after_initialize do
   if Rails.env.production?
-    ActiveRecord::Base.establish_connection(ENV['DATABASE_PUBLIC_URL'])
-    puts "✅ Conexão com PostgreSQL estabelecida em #{ActiveRecord::Base.connection_config[:host]}"
+    configs = ActiveRecord::Base.configurations.configs_for(env_name: 'production')
+
+    configs.each do |config|
+      begin
+        ActiveRecord::Base.establish_connection(config)
+        db_config = ActiveRecord::Base.connection_db_config
+        puts "✅ Conexão estabelecida com sucesso para #{config.name} (#{db_config.database})"
+      rescue => e
+        puts "❌ Falha na conexão com #{config.name}: #{e.message}"
+        raise "Database connection failed for #{config.name}"
+      end
+    end
   end
-rescue => e
-  puts "❌ Falha na conexão: #{e.message}"
-  raise
 end
